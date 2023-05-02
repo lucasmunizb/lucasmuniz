84592c96bc6704fe3fd6de80c825ffd92fa1b955
Lucas Muniz <lucasmunizbraga@gmail.com>
2023-05-02 19:08:16 -0300

    Liberando testes na API se o retorno é sucesso e se o retorno da API tem a posicao name e ibge_code

--- tests/Feature/API/UfControllerTest.php
+++ tests/Feature/API/UfControllerTest.php
@@ -4,9 +4,21 @@ namespace Tests\Feature\API;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
 class UfControllerTest extends TestCase
 {
-
+    public function test_the_application_returns_a_successful_response(): void
+    {
+        $response = $this->getJson('/api/cidades/mg');
+        $response->assertStatus(200);
+    }
+    public function test_the_json_return_node_name_and_ibge_code()
+    {
+        $response = $this->getJson('/api/cidades/mg');
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->hasAll(['0.name','0.ibge_code'])
+        );
+    }
 }
