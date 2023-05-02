04e0b92e39d931f716f90bbb90bbaa11b606ebc2
Lucas Muniz <lucasmunizbraga@gmail.com>
2023-05-02 19:18:17 -0300

    criando rota para buscar as cidades de acordo com UF informada no parametro da requisação

--- app/Http/Controllers/API/UfController.php
+++ app/Http/Controllers/API/UfController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UfController extends Controller
 {
-    //
+    public function show(string $uf)
+    {
+        $brasilApi = env('BRASIL_API');
+        $request = new Client();
+        try {
+            $response = $request->get("{$brasilApi}{$uf}");
+            $return = array_map(function ($arrayResponse){
+                return ['name'=>$arrayResponse['nome'],'ibge_code'=>$arrayResponse['codigo_ibge']];
+            },json_decode($response->getBody(),true));
+            return response()->json($return);
+        } catch (GuzzleException $e) {
+            return response()->json([
+               'erro' => $e->getMessage()
+            ], Response::HTTP_NO_CONTENT);
+        }
+    }
 }
